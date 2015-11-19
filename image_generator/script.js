1877c66d9b40b4e65ddb010be90a84e6e03caed0
debln128 <debln128@newschool.edu>
2015-11-19 23:42:57 +0000

    Add homepagetype.html

--- image_generator/script.js
+++ image_generator/script.js
@@ -1,9 +1,14 @@
 var RandomNumber = Math.round(Math.random() * 9+1 );
 var RandomNumber2 = Math.round(Math.random() * 9+1 );
 var RandomNumber3 = Math.round(Math.random() * 9+1 );
-var RandomNumber4 = Math.round(Math.random() * 5 );
-
-
+var RandomNumber4 = Math.round(Math.random() * 9+1 );
+var RandomNumber5 = Math.round(Math.random() * 9+1 );
+var RandomNumber6 = Math.round(Math.random() * 9+1 );
+var RandomNumber7 = Math.round(Math.random() * 9+1 );
+var RandomNumber8 = Math.round(Math.random() * 9+1 );
+var RandomNumber9 = Math.round(Math.random() * 9+1 );
+var RandomNumber10 = Math.round(Math.random() * 9+1 );
+var RandomNumber11 = Math.round(Math.random() * 9+1 );
 
 
 
@@ -11,11 +16,27 @@ $("#buttonDiv").click(function() {
 	RandomNumber = Math.round(Math.random() * 9+1 );
 	RandomNumber2 = Math.round(Math.random() * 9+1 );
 	RandomNumber3 = Math.round(Math.random() * 9+1 );
-	RandomNumber4 = Math.round(Math.random() * 5 );
-	$("#imageDiv").css("background-image", "url(img/shape" + RandomNumber + ".gif)");
-	$("#imageDiv2").css("background-image", "url(img/shape" + RandomNumber2 + ".gif)");
-	$("#imageDiv3").css("background-image", "url(img/shape" + RandomNumber3 + ".gif)");
-	$("#imageDiv4").css("background-image", "url(img/word" + RandomNumber4 + ".gif)");
+	RandomNumber4 = Math.round(Math.random() * 9+1 );
+	RandomNumber5 = Math.round(Math.random() * 9+1 );
+	RandomNumber5 = Math.round(Math.random() * 9+1 );
+	RandomNumber6 = Math.round(Math.random() * 9+1 );
+	RandomNumber7 = Math.round(Math.random() * 9+1 );
+	RandomNumber8 = Math.round(Math.random() * 9+1 );
+	RandomNumber9 = Math.round(Math.random() * 9+1 );
+	RandomNumber10 = Math.round(Math.random() * 9+1 );
+	RandomNumber11 = Math.round(Math.random() * 9+1 );
+	$("#imageDiv").css("background-image", "url(img/green" + RandomNumber + ".jpg)");
+	$("#imageDiv2").css("background-image", "url(img/green" + RandomNumber2 + ".jpg)");
+	$("#imageDiv3").css("background-image", "url(img/green" + RandomNumber3 + ".jpg)");
+	$("#imageDiv4").css("background-image", "url(img/green" + RandomNumber4 + ".jpg)");
+	$("#imageDiv5").css("background-image", "url(img/green" + RandomNumber5 + ".jpg)");
+	$("#imageDiv6").css("background-image", "url(img/green" + RandomNumber5 + ".jpg)");
+	$("#imageDiv7").css("background-image", "url(img/green" + RandomNumber5 + ".jpg)");
+	$("#imageDiv8").css("background-image", "url(img/green" + RandomNumber5 + ".jpg)");
+	$("#imageDiv9").css("background-image", "url(img/green" + RandomNumber5 + ".jpg)");
+	$("#imageDiv10").css("background-image", "url(img/green" + RandomNumber5 + ".jpg)");
+	$("#imageDiv11").css("background-image", "url(img/green" + RandomNumber5 + ".jpg)");
+	
 	
 	// console.log("it worked!");
 	
@@ -29,4 +50,4 @@ $("#buttonDiv").click(function() {
 
 console.log(RandomNumber);
 
-$("body").css("background-image", "url(img/shape3.gif)");
+$("body").css("background-image", "url(img/background.jpg)");
